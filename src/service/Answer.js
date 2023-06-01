3c772f84180ae2cc08fbf987241e81e5e61d84c6
LeMinhNhanB1910268 <vlthatbadao@gmail.com>
2023-06-01 15:39:53 +0700

    update env

--- src/service/Answer.js
+++ src/service/Answer.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 import createApiClient from "./apiService";
 // const api = createApiClient('https://api.openai.com/v1/chat/completions');
-
+require('dotenv').config();
 const AnswerOpenAI = async(data) => {
+    const apiKey = process.env.OPENAI_API_KEY;
     return (await axios.post('https://api.openai.com/v1/chat/completions',data,{
         headers:{
-            Authorization: `Bearer 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`
+            Authorization: `Bearer ${apiKey}`
+            // Authorization: `Bearer OPEN_AI_KEY`
         }
     })).data
 }
